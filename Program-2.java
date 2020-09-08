ea7fd08c2f17dbf2ca9d3710e872b5dfb9d8a3e5
pacific95MX <65505981+pacific95MX@users.noreply.github.com>
2020-09-08 13:37:02 +0530

    Update Program-2.java

--- Program-2.java
+++ Program-2.java
@@ -1,1 +1,25 @@
-import.java.util.Scanner;
+import java.util.Scanner;
+public class Pro {
+
+   public static void main(String[] args)
+
+{
+   int i,n,sum=0;
+
+   System.out.print("Input value of a: ");
+{
+   Scanner s = new Scanner(System.in);
+		    n = s.nextInt();
+  System.out.print("Outputs are:");
+   for(i=1;i<=n;i++)
+
+   {
+     System.out.print(2*i-1);
+     if(i<n){
+         
+           System.out.print(",");
+         }       
+       }  
+     }
+    }
+}
